4da7d9606915f833cd37172a3d235e98f4184829
Cristian Costa <cristiancosta1991@gmail.com>
2015-09-11 11:39:07 -0300

    Commit inicial.

---
+++ Proyecto/test.bat
@@ -0,0 +1,3 @@
+cd C:\Archivos de Programa\Unity\Editor
+del C:\Users\Mariel\Documents\Cocteles\UnitTestResults.xml
+Unity.exe -quit -batchmode -projectPath C:\Users\Mariel\Documents\Cocteles -executeMethod UnityTest.Batch.RunUnitTests -filter=CervezaTest.envaseTest
